f6d205367e61b950b9146fa2f3d1143727467e4b
JUSSIAR <klokov.stas.022@mail.ru>
2022-10-22 17:08:57 +0300

    Added router basic

--- src/common/components/footer/footer.tsx
+++ src/common/components/footer/footer.tsx
@@ -1,1 +1,11 @@
-export {}
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <h5> Meta/About/Contact </h5>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/home.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Page from "../apps/page/page";
+
+function HomePage() {
+  return (
+    <Page>
+      <div> Home Page </div>
+    </Page>
+  )
+}
+
+export default HomePage
