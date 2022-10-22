96a263b0f59b494806fa9679c113027f35083818
JUSSIAR <klokov.stas.022@mail.ru>
2022-10-22 19:32:24 +0300

    Some structured fixes

--- src/entry/entry.tsx
+++ src/entry/entry.tsx
@@ -8,27 +8,27 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 function Entry() {
 
-  if (process.env.NODE_ENV !== 'development') {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route index path='/' element={<ErrorPage/>}/>
-          <Route path='*' element={<Navigate to='/'/>}/>
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+    if (process.env.NODE_ENV !== 'development') {
+        return (
+            <BrowserRouter>
+                <Routes>
+                    <Route index path='/' element={<ErrorPage/>}/>
+                    <Route path='*' element={<Navigate to='/'/>}/>
+                </Routes>
+            </BrowserRouter>
+        );
+    }
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path='/' element={<HomePage/>}/>
-        <Route path='/profile/:uid' element={<ProfilePage/>}/>
-        <Route path='/404' element={<ErrorPage/>}/>
-        <Route path='*' element={<Navigate to='/404'/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index path='/' element={<HomePage/>}/>
+                <Route path='/profile/:uid' element={<ProfilePage/>}/>
+                <Route path='/404' element={<ErrorPage/>}/>
+                <Route path='*' element={<Navigate to='/404'/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default Entry;
