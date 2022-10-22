2e9e093573d8970b7fd25579736bafead05b463b
JUSSIAR <klokov.stas.022@mail.ru>
2022-10-22 18:12:04 +0300

    Changed environment resolving

--- src/entry/entry.tsx
+++ src/entry/entry.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import './entry.css';
 
+// For development mode
 import HomePage from '../pages/home.page';
 import ProfilePage from '../pages/profile.page';
-// import ErrorPage from '../pages/error.page';
-
+import ErrorPage from '../pages/error.page';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import ErrorPage from "../pages/error.page";
+
+// For production mode
+import FailStatusView from '../common/components/fail-status-view/fail-status-view';
+import { FailType } from '../common/components/fail-status-view/types';
 
 function Entry() {
+
+  if (process?.env?.NODE_ENV === 'development' || process?.env?.STAGE === 'dev') {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route index path='/' element={<HomePage/>}/>
+          <Route path='/profile/:uid' element={<ProfilePage/>}/>
+          <Route path='/404' element={<ErrorPage/>}/>
+          <Route path='*' element={<Navigate to='/404'/>}/>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path='/' element={<HomePage/>}/>
-        <Route path='/profile/:uid' element={<ProfilePage/>}/>
-        <Route path='/404' element={<ErrorPage/>}/>
-        <Route path='*' element={<Navigate to='/404'/>}/>
-      </Routes>
-    </BrowserRouter>
+    <FailStatusView failType={FailType.ServiceUnavalible}/>
   );
 }
 
